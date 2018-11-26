9113f4767af83a39a4b9f4c5991fbdab5a2e7525
Sateek Roy <sateeksuper@gmail.com>
2018-11-25 22:21:00 -0500

    Updates

---
+++ precache-manifest.f91c3a4cde1f87ca45fc1caf2066e7b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "64202363848f7ad8d988",
+    "url": "/contact-manager/static/js/main.64202363.chunk.js"
+  },
+  {
+    "revision": "fe6c9c0fe749aca9f4cb",
+    "url": "/contact-manager/static/js/1.fe6c9c0f.chunk.js"
+  },
+  {
+    "revision": "64202363848f7ad8d988",
+    "url": "/contact-manager/static/css/main.f5741332.chunk.css"
+  },
+  {
+    "revision": "fe6c9c0fe749aca9f4cb",
+    "url": "/contact-manager/static/css/1.15aa35ae.chunk.css"
+  },
+  {
+    "revision": "f60e08d6969ff33481a391d6dce91a02",
+    "url": "/contact-manager/index.html"
+  }
+];
